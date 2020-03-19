445822b0695c7cfeec3263bee5410d9d0a203022
Sardaan <sardaaaaana2345@gmail.com>
2020-03-19 22:53:31 +0300

    Маршрутизация

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 
-import rootReducer from './store/reducers';
-import {Provider} from 'react-redux';
-import {createStore} from "redux";
 import AuthContainer from "./components/AuthContainer";
 import RegistrationContainer from "./components/RegistrationContainer";
 
@@ -10,13 +7,14 @@ import RegistrationContainer from "./components/RegistrationContainer";
 export default class App extends React.Component{
     render() {
         return(
-            <Provider store = {createStore(rootReducer)}>
+
             <div className="forms">
                 <AuthContainer/>
                 <RegistrationContainer/>
             </div>
-            </Provider>
+
         )
     }
 }
+
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,21 @@
 import ReactDOM from "react-dom";
-import App from "./App";
 import React from "react";
-ReactDOM.render(<App/>,document.getElementById("root"));
+import App from "./App";
+import Home from "./components/Home";
+
+import rootReducer from './store/reducers';
+import {Provider} from 'react-redux';
+import {createStore} from "redux";
+
+import {Route, Router} from "react-router-dom";
+import {createBrowserHistory} from "history";
+
+ReactDOM.render(
+    <Provider store = {createStore(rootReducer)}>
+        <Router history={createBrowserHistory()}>
+            <Route exact path='/' component={App}/>
+            <Route path='/home' component={Home}/>
+        </Router>
+    </Provider>
+    , document.getElementById('root')
+);
